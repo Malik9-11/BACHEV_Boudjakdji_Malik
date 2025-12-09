f5be08ce49910c2c2210de00ab1b90c374f2eccf
Moh445-cmd <youchaa94@gmail.com>
2025-12-09 11:21:13 +0000

    affichage tableau et section de sequences

--- malik.py
+++ malik.py
@@ -1,4 +1,11 @@
-print("hello")
-# test
 import pandas as pd
-print("hi")
+data = {
+    "Séquence": ["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGC", "TTAACCGGAT"],
+    "Longueur": [11, 12, 12, 10, 11, 10, 10],
+    "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
+}
+
+df = pd.DataFrame(data)
+print(df)
+sequences = df["Séquence"]
+print(sequences)
