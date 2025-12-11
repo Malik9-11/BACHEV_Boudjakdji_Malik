b5d657bce7be5abfd1129338c9b4b4b6627f319e
Malik9-11 <maliktankix@gmail.com>
2025-12-11 16:55:50 +0000

    modifications

--- malik.py
+++ malik.py
@@ -4,20 +4,33 @@
 # Salhi Amira Nour El Houda
 # Nordine Baya
 
+#importation de la bibliothèque pandas
 import pandas as pd
+
+# Données : Séquences ADN, Longueur, Pourcentage de GC
 data = {
     "Séquence": ["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGC", "TTAACCGGAT"],
     "Longueur": [11, 12, 12, 10, 11, 10, 10],
     "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
 }
 
+# Création du DataFrame (tableau pandas)
 df = pd.DataFrame(data)
-print(df)
+print("******** Creation et affichage ******** \n")
+
+# Affichage du tableau
+print("Tableau des séquences ADN : \n")
+print(df, "\n")
+
+# Opérations sur les tableaux:
+print("******** Operations ******** \n")
+# Sélectionner la colonne "Longueur"
+print("Affichage de la colonne Longueur: \n")
 Longueur = df["Longueur"]
-print(Longueur)
+print(Longueur, "\n")
 
 # Filtrer les séquences dont la longueur est supérieure à 10
-print ("******** Séquences avec longueur > 10 ********")
+print ("******** Séquences avec longueur > 10 ********\n")
 # Filtrer les séquences avec une longueur supérieur à 10
 filtered_df = df[df["Longueur"] > 10]
 print(filtered_df, "\n")
@@ -26,4 +39,4 @@ print(filtered_df, "\n")
 print ("******** Calcul de la moyenne ********")
 # Calculer la moyenne du pourcentage de GC
 average_gc = df["Pourcentage GC"].mean()
-print(f"\n Pourcentage moyen de GC : {average_gc:.3f}%")
+print(f"\n Pourcentage moyen de GC : {average_gc:.3f}%\n")
